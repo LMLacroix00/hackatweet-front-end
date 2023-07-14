74142bf89627d45cfed6656f927fdee5797df8e2
Your name <Your email>
2023-07-14 10:33:43 +0200

    frontend

--- components/Login.js
+++ components/Login.js
@@ -1,19 +1,20 @@
 import Signin from "/components/Signin.js";
 import SignUp from "/components/SignUp.js";
-import React from " react";
-import Modal from " @materail-ui/core/Modal";
+import React from "react";
+
+import styles from "../styles/Login.module.css";
 
 function Login() {
   return (
     <div>
       <main className={styles.main}>
         <img className={styles.logo} src="logo.jpeg" />
-        <img className={style.background} src="background.png" />
+        <img className={styles.background} src="background.png" />
         <h1 className={styles.title}>See what's happening</h1>
         <h3> JOIN HACKATWEET TODAY </h3>
-        <button> sign up </button>
+        <button className={styles.buttonUp}> sign up </button>
         <h4> Already have an account ?</h4>
-        <button> sign in </button>
+        <button className={styles.buttonIn}> sign in </button>
       </main>
     </div>
   );
