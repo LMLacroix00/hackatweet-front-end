f665fd883f44e80b1b325e0029708eee7f1439e3
lucius-César <luciendelgrange5@gmail.com>
2023-07-14 12:22:28 +0200

    connection mechanic setup

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -2,6 +2,10 @@ import React from "react";
 import styles from '../styles/LoginScreen.module.css';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { login, logout } from '../reducers/user';
+import Link from 'next/link';
+
+
 
 
 
@@ -50,36 +54,46 @@ function LoginScreen() {
 			});
 	};
 
-  if (user.token) {
-    console.log("user connected")
+if(user.token){
+  return(
+    <div>
+      <Link href="/home"> You are logged In </Link>
+    </div>
+
+  )
+  }
+
+  else{
+    return (
+    
+      <div className = {styles.loginScreenParentContainer}>
+        <div className = {styles.loginScreenLeftPart}> 
+          <img className = {styles.twitterLogo} src = "logo.png"/>
+        </div>
+        <div className = {styles.loginScreenRightPart}> 
+          <h1 className={styles.title}>See what's happening</h1>
+          <h2> JOIN HACKATWEET TODAY </h2>
+          <div className = {styles.signUp}>
+              <input type="text" placeholder="Firstname" id="signUpfirstName" onChange={(e) => setSignUpFirstname(e.target.value)} value={signUpFirstname}/>
+              <input type="text" placeholder="Username" id="signUpUsername"  onChange={(e) => setSignUpUsername(e.target.value)} value={signUpUsername}/>
+              <input type="password" placeholder="Password" id="signUpPassword" onChange={(e) => setSignUpPassword(e.target.value)} value={signUpPassword}/>
+              <button id="register" onClick = {handleSignUp} >Sign up</button>
+          </div>
+          <div className = {styles.signIn}>
+            <h4> Already have an account ?</h4>
+            <input type="text" placeholder="Username" id="signUpUsername" onChange={(e) => setSignInUsername(e.target.value)} value={signInUsername}/>
+            <input type="password" placeholder="Password" id="signUpPassword" onChange={(e) => setSignInPassword(e.target.value)} value={signInPassword}/>
+            <button id="register" onClick = {handleSignIn}>Sign in</button>
+          </div>
+        </div>
+          
+      </div>
+    )
   }
 
 
-  return (
-    
-    <div className = {styles.loginScreenParentContainer}>
-      <div className = {styles.loginScreenLeftPart}> 
-        <img className = {styles.twitterLogo} src = "logo.png"/>
-      </div>
-      <div className = {styles.loginScreenRightPart}> 
-        <h1 className={styles.title}>See what's happening</h1>
-        <h2> JOIN HACKATWEET TODAY </h2>
-        <div className = {styles.signUp}>
-            <input type="text" placeholder="Firstname" id="signUpfirstName" onChange={(e) => setSignUpFirstname(e.target.value)} value={signUpFirstname}/>
-            <input type="text" placeholder="Username" id="signUpUsername"  onChange={(e) => setSignUpUsername(e.target.value)} value={signUpUsername}/>
-            <input type="password" placeholder="Password" id="signUpPassword" onChange={(e) => setSignUpPassword(e.target.value)} value={signUpPassword}/>
-            <button id="register" onClick = {handleSignUp} >Sign up</button>
-        </div>
-        <div className = {styles.signIn}>
-          <h4> Already have an account ?</h4>
-          <input type="text" placeholder="Username" id="signUpUsername" onChange={(e) => setSignInUsername(e.target.value)} value={signInUsername}/>
-          <input type="password" placeholder="Password" id="signUpPassword" onChange={(e) => setSignInPassword(e.target.value)} value={signInPassword}/>
-					<button id="register" onClick = {handleSignIn}>Sign in</button>
-        </div>
-      </div>
-        
-    </div>
-  );
+
+
 }
 
 export default LoginScreen;
